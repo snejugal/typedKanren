aaf13aa71e033de477193cf1073e6a41b3d24567
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2024-03-20 10:50:26 +0300

    Sketch better matching

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -243,7 +245,7 @@ instance Num (Term a) => Num (ValueOrVar a) where
 
 instance Unifiable Int
 instance Unifiable Bool
-instance Eq (Term a) => Unifiable (ValueOrVar a)
+instance Eq (Term a) => Unifiable (ValueOrVar a)  -- ???
 
 addSubst :: Unifiable a => (VarId a, ValueOrVar a) -> State -> State
 addSubst (VarId i, value) State{knownSubst = Subst m, ..} = State
@@ -297,7 +299,7 @@ run f = Foldable.toList (fmap resolveQueryVar (runGoal (f queryVar) initialState
     queryVar = Var (VarId 0)
     resolveQueryVar State{..} = apply knownSubst queryVar
 
-fresh' :: (ValueOrVar a -> Goal ()) -> Goal ()
+fresh' :: (ValueOrVar a -> Goal b) -> Goal b
 fresh' f = Goal $ \State{..} ->
   let newState = State
         { maxVarId = maxVarId + 1
@@ -357,3 +359,75 @@ allo p xs =
     LCons y ys -> do
       p y
       allo p ys
+
+data Pair a = Pair a a
+  deriving (Eq, Show, Generic)
+
+data LogicPair a = LogicPair (ValueOrVar a) (ValueOrVar a)
+  deriving (Generic)
+
+deriving instance (Eq (Term a)) => Eq (LogicPair a)
+deriving instance (Show (Term a)) => Show (LogicPair a)
+
+instance Unifiable a => Unifiable (Pair a) where
+  type Term (Pair a) = LogicPair a
+  subst = genericSubst
+  unify = genericUnify
+  inject = genericInject
+  extract = genericExtract
+
+matchLogicPair :: Unifiable a => ValueOrVar (Pair a) -> Maybe (Goal (Term (Pair a)))
+matchLogicPair (Value t@LogicPair{}) = Just (return t)
+matchLogicPair var@Var{} = Just $
+  fresh' $ \x ->
+    fresh' $ \y -> do
+      let t = LogicPair x y
+      var === Value t
+      return t
+
+pattern LPair' :: Unifiable a => Goal (LogicPair a) -> ValueOrVar (Pair a)
+pattern LPair' lpair <- (matchLogicPair -> Just lpair)
+
+both :: Unifiable a => (ValueOrVar a -> Goal ()) -> ValueOrVar (Pair a) -> Goal ()
+both p = \case
+  LPair' xy -> xy >>= \case
+    LogicPair x y -> do
+      p x
+      p y
+
+matchLNil :: Unifiable a => ValueOrVar [a] -> Maybe (Goal (Term [a]))
+matchLNil = \case
+  Value t@LNil -> Just (return t)
+  var@Var{} -> Just $ do
+    let t = LNil
+    var === Value t
+    return t
+  _ -> Nothing
+
+matchLCons :: Unifiable a => ValueOrVar [a] -> Maybe (Goal (Term [a]))
+matchLCons = \case
+  Value t@LCons{} -> Just (return t)
+  var@Var{} -> Just $ do
+    fresh' $ \x ->
+      fresh' $ \xs -> do
+        let t = LCons x xs
+        var === Value t
+        return t
+  _ -> Nothing
+
+pattern LNil' :: Unifiable a => Goal (Term [a]) -> ValueOrVar [a]
+pattern LNil' g <- (matchLNil -> Just g)
+
+pattern LCons' :: Unifiable a => Goal (Term [a]) -> ValueOrVar [a]
+pattern LCons' g <- (matchLCons -> Just g)
+
+allo' :: Unifiable a => (ValueOrVar a -> Goal ()) -> ValueOrVar [a] -> Goal ()
+allo' p = \case
+  LNil' g -> g >>= \case
+    LNil -> return ()
+    _ -> error "impossible"
+  LCons' g -> g >>= \case
+    LCons y ys -> do
+      p y
+      allo' p ys
+    _ -> error "impossible"
