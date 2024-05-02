bce747d24df27b64c89019c44869e4afb57bbfef
Artem Starikov <contact@snejugal.ru>
2024-05-02 17:03:31 +0300

    Add one more example with trees

--- app/Tree/Main.hs
+++ app/Tree/Main.hs
@@ -31,5 +31,31 @@ treeo = matche
       treeo left
       treeo right)
 
+inverto :: Unifiable a => ValueOrVar (Tree a) -> ValueOrVar (Tree a) -> Goal ()
+inverto tree inverted = tree & (matche
+  & on _LogicEmpty (\() -> inverted === Value LogicEmpty)
+  & on _LogicNode (\(value, left, right) ->
+      fresh $ \(invertedLeft, invertedRight) -> do
+        inverted === Value (LogicNode value invertedLeft invertedRight)
+        inverto left invertedRight
+        inverto right invertedLeft))
+
+trees :: [ValueOrVar (Tree Int)]
+trees = run treeo
+
+--    4
+--  2   6
+-- 1 3 5 7
+example :: Tree Int
+example = Node 4
+            (Node 2 (leaf 1) (leaf 3))
+            (Node 6 (leaf 5) (leaf 7))
+  where leaf x = Node x Empty Empty
+
 main :: IO ()
-main = mapM_ print $ extract' <$> take 10 (run treeo)
+main = do
+  putStrLn "trees:"
+  mapM_ print $ extract' <$> take 10 (run treeo)
+
+  putStrLn "inverto example:"
+  mapM_ print $ extract' <$> run (inverto (inject' example))
