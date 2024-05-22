5b45badf12b9c94345497a9312eb266f27675a21
Artem Starikov <contact@snejugal.ru>
2024-05-22 22:23:33 +0300

    Implement expontentiation on binary numbers

--- example/Binary.hs
+++ example/Binary.hs
@@ -22,6 +22,7 @@ module Binary (
   lesso,
   mulo,
   divo,
+  logo,
   example,
 ) where
 
@@ -143,6 +144,15 @@ lessl3o a x y z = a & (matche'
             lessl3o ar xr y zr)
         & on' _LogicCons' (\(_, yr) -> lessl3o ar xr yr z)
         & enter'))
+  & enter')
+
+appendo :: (Logical a) => Term [a] -> Term [a] -> Term [a] -> Goal ()
+appendo xs ys zs = xs & (matche'
+  & on' _LogicNil' (\() -> ys === zs)
+  & on' _LogicCons' (\(x, xs') -> do
+      zs' <- fresh
+      zs === Value (LogicCons x zs')
+      appendo xs' ys zs')
   & enter')
 {- FOURMOLU_ENABLE -}
 
@@ -344,6 +354,166 @@ splito n r n1 n2 =
         poso n2'
         n2 === Value (LogicCons b n2')
         splito n' r' n1 n2'
+    ]
+
+{- FOURMOLU_DISABLE -}
+-- | Calculate `n = (b + 1)^q`, where `b + 1` is a power of two.
+exp2o :: Term Binary -> Term Binary -> Term Binary -> Goal ()
+exp2o n b = matche'
+  & on' _LogicNil' (\() -> n === inject' [I])
+  & on' _LogicCons' (\(q, q1) -> q & (matche'
+      & on' _LogicO' (\() -> do
+          poso q1
+          lesslo b n
+          b2 <- fresh
+          appendo b (Value (LogicCons (inject' I) b)) b2
+          exp2o n b2 q1)
+      & on' _LogicI' (\() -> q1 & (matche'
+        & on' _LogicNil' (\() -> do
+            gtlo n
+            _n2 <- fresh
+            splito n b (inject' [I]) _n2)
+        & on' _LogicCons' (\_ -> do
+            (n1, _n2) <- fresh
+            poso n1
+            splito n b n1 _n2
+            b2 <- fresh
+            appendo b (Value (LogicCons (inject' I) b)) b2
+            exp2o n1 b2 q1)
+        & enter'))
+      & enter'))
+  & enter'
+{- FOURMOLU_ENABLE -}
+
+-- | Calculate `nq = n ^ q`.
+repeatedMulo :: Term Binary -> Term Binary -> Term Binary -> Goal ()
+repeatedMulo n q nq =
+  disjMany
+    [ do
+        poso n
+        zeroo q
+        nq === inject' [I]
+    , do
+        q === inject' [I]
+        n === nq
+    , do
+        gtlo q
+        (q1, nq1) <- fresh
+        addo q1 (inject' [I]) q1
+        repeatedMulo n q1 nq1
+        mulo nq1 n nq
+    ]
+
+-- | Calculate discrete logarithm `q` of a number `n` in base `b`, perhaps with
+-- some remainder `r`.
+--
+-- >>> bimap extract' extract' <$> run (\(q, r) -> logo (inject' 9) (inject' 3) q r)
+-- [(Just [O,I],Just [])]
+-- >>> bimap extract' extract' <$> run (\(q, r) -> logo (inject' 15) (inject' 3) q r)
+-- [(Just [O,I],Just [0,I,I])]
+--
+-- One can turn this around to find the number `b` raised to some power `q`:
+--
+-- >>> extract' <$> run (\n -> logo n (inject' 5) (inject' 2) (inject' 0))
+-- [Just [I,O,O,I,I]]
+--
+-- or to find the `q`-th root of `n`:
+--
+-- >>> extract' <$> run (\b -> logo (inject' 8) b (inject' 3) (inject' 0))
+-- [Just [O,I]]
+--
+-- If you want to enumerate solutions to `logo n b q r`, you probably shouldn't
+-- go beyond the first 16 solutions without an OOM killer.
+--
+-- The original implementation of this relation in Prolog can be found at
+-- <https://okmij.org/ftp/Prolog/Arithm/pure-bin-arithm.prl>. Although the paper
+-- mentions this relation in section 6, it does not discuss its implementation
+-- in detail.
+logo
+  :: Term Binary
+  -- ^ `n`, of which to calculate the logarithm
+  -> Term Binary
+  -- ^ `b`, the base
+  -> Term Binary
+  -- ^ `q`, the logarithm
+  -> Term Binary
+  -- ^ `r`, the remainder
+  -> Goal ()
+logo n b q r =
+  disjMany
+    [ do
+        -- n = b^0 + 0, b > 0
+        n === inject' [I]
+        poso b
+        zeroo q
+        zeroo r
+    , do
+        -- n = b^0 + r, r > 0
+        zeroo q
+        lesso n b
+        poso r
+        addo r (inject' [I]) n
+    , do
+        -- n = b^1 + r, b >= 2
+        q === inject' [I]
+        gtlo b
+        samelo n b
+        addo r b n
+    , do
+        -- n = 1^q + r, q > 0
+        b === inject' [I]
+        poso q
+        addo r (inject' [I]) n
+    , do
+        -- n = 0^q + r, q > 0
+        zeroo b
+        poso q
+        n === r
+    , do
+        -- n = 2^q + r, n >= 4
+        b === inject' [O, I]
+        (_b1, _b2, n1) <- fresh
+        poso n1
+        n === Value (LogicCons _b1 (Value (LogicCons _b2 n1)))
+        exp2o n (inject' []) q
+        _r1 <- fresh
+        splito n n1 _r1 r
+    , do
+        -- n = b^q + r, b >= 3
+        b === inject' [I, I] `disj` do
+          (b1, b2, b3, br) <- fresh
+          b === Value (LogicCons b1 (Value (LogicCons b2 (Value (LogicCons b3 br)))))
+        lesslo b n
+
+        (bw1, bw) <- fresh
+        exp2o b (inject' []) bw1
+        addo bw1 (inject' [I]) bw
+
+        (q1, bwq1, nw1, nw) <- fresh
+        lesslo q n
+        addo q (inject' [I]) q1
+        mulo bw q1 bwq1
+        lesso nw1 bwq1
+        exp2o n (inject' []) nw1
+        addo nw1 (inject' [I]) nw
+
+        (ql, ql1, _r, bql) <- fresh
+        divo nw bw ql1 _r
+        samelo q ql `disj` lesslo ql q
+        repeatedMulo b ql bql
+
+        (_r', qh, qdh, qd) <- fresh
+        divo nw bw1 qh _r'
+        addo ql qdh qh
+        addo ql qd q
+        qd === qdh `disj` lesso qd qdh
+
+        (bqd, bq, bq1) <- fresh
+        repeatedMulo b qd bqd
+        mulo bql bqd bq
+        mulo b bq bq1
+        addo bq r n
+        lesso n bq1
     ]
 
 trimap :: (a -> b) -> (a, a, a) -> (b, b, b)
