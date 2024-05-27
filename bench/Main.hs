a70ecfcab64fcbbe64947d441f8c7a0a83c2fb46
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2024-05-27 13:12:11 +0300

    Add log (base 3) benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -9,6 +9,9 @@ import           Criterion.Main
 exp3o :: Binary -> Term Binary -> Goal ()
 exp3o n e3n = Binary.logo e3n (inject' 3) (inject' n) (inject' 0)
 
+log3o :: Binary -> Term Binary -> Goal ()
+log3o n log3n = Binary.logo (inject' n) (inject' 3) log3n (inject' 0)
+
 whnfGoalOnce :: Fresh v => (a -> v -> Goal ()) -> a -> Benchmarkable
 whnfGoalOnce f = whnf $ \x ->
   case run (f x) of
@@ -16,9 +19,14 @@ whnfGoalOnce f = whnf $ \x ->
     r:_ -> Just r
 
 main :: IO ()
-main = defaultMain [
-  bgroup "3^n"
+main = defaultMain
+  [ bgroup "3^n "
     [ bench (" n=" <> show n) $ whnfGoalOnce exp3o (fromIntegral n)
-    | n <- [1..5 :: Int]
+    | n <- [0..2 :: Int]
+    ]
+  , bgroup "log_3 n "
+    [ bench (" n=" <> show n) $ whnfGoalOnce log3o (fromIntegral n)
+    | p <- [0..2 :: Int]
+    , let n = 3^p :: Int
     ]
   ]
