1d5ae7e5b439c69152338172d55b776099a0c2bd
Artem Starikov <contact@snejugal.ru>
2024-06-09 23:59:17 +0300

    Special-case `Yield x Done` as `Only x`

--- src/Kanren/Stream.hs
+++ src/Kanren/Stream.hs
@@ -7,6 +7,7 @@ import           Prelude hiding (take)
 
 data Stream a
   = Done
+  | Only a
   | Yield a (Stream a)
   | Await (Stream a)
   deriving (Functor, Foldable)
@@ -15,19 +16,22 @@ instance (Show a) => Show (Stream a) where
   show ys = "[" ++ show' ys
    where
     show' Done                     = "]"
+    show' (Only x)                 = show x ++ "]"
     show' (Yield x xs@(Yield _ _)) = show x ++ ", " ++ show' xs
     show' (Yield x xs)             = show x ++ show' xs
     show' (Await xs)               = show xs    -- FIXME: demonstrate the use of Await
 
 instance Applicative Stream where
-  pure x = Yield x Done
+  pure = Only
 
   Done <*> _        = Done
+  Only f <*> xs     = fmap f xs
   Yield f fs <*> xs = interleave (fmap f xs) (fs <*> xs)
   Await fs <*> xs   = Await (fs <*> xs)
 
 instance Monad Stream where
   Done >>= _       = Done
+  Only x >>= f     = f x
   Yield x xs >>= f = interleave (f x) (xs >>= f)
   Await xs >>= f   = Await (xs >>= f)
 
@@ -37,5 +41,6 @@ maybeToStream (Just x) = pure x
 
 interleave :: Stream a -> Stream a -> Stream a
 interleave Done ys         = ys
+interleave (Only x) ys     = Yield x ys
 interleave (Yield x xs) ys = Yield x (interleave ys xs)
 interleave (Await xs) ys   = Await (interleave ys xs)
