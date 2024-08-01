71df90f3d15fa809565f2a26a405d6a48cf07a55
Artem Starikov <contact@snejugal.ru>
2024-08-01 09:35:58 +0300

    Remove `m` from `Yield`

--- src/Kanren/Stream.hs
+++ src/Kanren/Stream.hs
@@ -16,7 +16,7 @@ import Prelude hiding (take)
 data StreamT m a
   = Done
   | Only a
-  | Yield a (m (StreamT m a))
+  | Yield a (StreamT m a)
   | Await (m (StreamT m a))
   | M (m (StreamT m a))
   deriving (Functor)
@@ -28,7 +28,7 @@ instance (Monad m) => Applicative (StreamT m) where
 instance (Monad m) => Monad (StreamT m) where
   Done >>= _ = Done
   Only x >>= f = f x
-  Yield x xs >>= f = M (reduceM (xs <&> (interleave (f x) . (>>= f))))
+  Yield x xs >>= f = interleave (f x) (xs >>= f)
   Await xs >>= f = Await (reduceM (xs <&> (>>= f)))
   M xs >>= f = M (reduceM (xs <&> (>>= f)))
 
@@ -44,8 +44,8 @@ maybeToStream (Just x) = Only x
 
 interleave :: (Applicative m) => StreamT m a -> StreamT m a -> StreamT m a
 interleave Done ys = ys
-interleave (Only x) ys = Yield x (pure ys)
-interleave (Yield x xs) ys = Yield x (interleave ys <$> xs)
+interleave (Only x) ys = Yield x ys
+interleave (Yield x xs) ys = Yield x (interleave ys xs)
 interleave (Await xs) ys = Await (interleave ys <$> xs)
 interleave (M xs) ys = M (xs <&> (`interleave` ys))
 
@@ -54,7 +54,7 @@ toList _ Done = pure []
 toList f (Only x) = f x <&> (: [])
 toList f (Yield x xs) = do
   x' <- f x
-  xs' <- xs >>= toList f
+  xs' <- toList f xs
   return (x' : xs')
 toList f (Await xs) = xs >>= toList f
 toList f (M xs) = xs >>= toList f
@@ -63,6 +63,6 @@ take :: (Functor m) => Int -> StreamT m a -> StreamT m a
 take n _ | n <= 0 = Done
 take _ Done = Done
 take _ (Only x) = Only x
-take n (Yield x xs) = Yield x (take (n - 1) <$> xs)
+take n (Yield x xs) = Yield x (take (n - 1) xs)
 take n (Await xs) = Await (take n <$> xs)
 take n (M xs) = M (take n <$> xs)
