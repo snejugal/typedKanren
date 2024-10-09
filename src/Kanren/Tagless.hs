69742593cbb26fcb13fb10da8afc132bbd18c28d
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2024-10-09 17:55:35 +0300

    Add docs for Tagless with references

--- src/Kanren/Tagless.hs
+++ src/Kanren/Tagless.hs
@@ -1,5 +1,29 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE MagicHash    #-}
+-- | "Tagless" sum types allow to skip explicit tag for at least one of the alternatives in a sum type,
+-- assuming that we can differentiate between the alternatives at runtime by other means.
+--
+-- The benefit of this representation is pronounced for recursive data types:
+--
+-- * In the fastest miniKanren implementations, this approach is used
+--   to avoid expensive recursive injection of a regular (non-logical) value into a miniKanren program.
+--   See Section 6 of [1] for more details about typed implementation of tagless approach in OCanren.
+--   Note that faster-minikanren implementation (in Scheme/Racket) is also using a similar technique in an untyped setting.
+--
+-- * An efficient bignum representation can be done by "unboxing" the (small) integer constructor.
+--   See the details of this example in [2].
+--
+-- This "tagless" representation approach is a variation of
+-- * niche filling optimization [3] (which can be found in Rust, for instance)
+-- * unboxed constructors [2]
+--
+-- See also "flat Maybe" implementation by Andras Kovacs: <https://github.com/AndrasKovacs/flat-maybe>
+--
+-- Note that this approach is NOT the same as unboxed sum typed in Haskell (at least not in their current form).
+--
+-- [1] Dmitrii Kosarev, Dmitry Boulytchev. Typed Embedding of a Relational Language in OCaml <https://arxiv.org/abs/1805.11006>
+-- [2] Nicolas Chataing, Stephen Dolan, Gabriel Scherer, and Jeremy Yallop. 2024. Unboxed Data Constructors: Or, How cpp Decides a Halting Problem. Proc. ACM Program. Lang. 8, POPL, Article 51 (January 2024), 31 pages. https://doi.org/10.1145/3632893
+-- [3] Bartell-Mangel, N. L. (2022). Filling a Niche: Using Spare Bits to Optimize Data Representations.
 module Kanren.Tagless where
 
 import           Control.DeepSeq   (NFData (rnf))
